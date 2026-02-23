0733f90ae8eacf7b26175eb84bc00d8755aa0233
sumandas131299 <sumandas131299@gmail.com>
2026-02-23 17:14:18 +0530

    requirements commit

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import os
+
 import main.app as app_module
 app = app_module.create_app()
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)    
